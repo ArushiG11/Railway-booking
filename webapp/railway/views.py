7f1350c9f815701468c371e226dbeda19659cef3
yy676 <168886090+yy676@users.noreply.github.com>
2024-11-07 22:16:05 -0500

    Add files via upload

--- webapp/railway/views.py
+++ webapp/railway/views.py
@@ -4,17 +4,37 @@ from django.db import connection
 # Remember to write a view for every single page, then link the function in railway/urls.py to display the view
 
 # view for the index page
+
 def index(request):
     example = "hello"
-    # let's test the mySQL database connection
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT username, user_password FROM loginTest")
-        login_info = cursor.fetchall()
-    # If you want to pass data into the html files, you include them here in context. 
-    # Context is in JSON so you can add multiple pieces of information
-    # check index.html to see how to display the information
-    context = {
-        'example': example,
-        'login_info': [{'username': row[0], 'user_password': row[1]} for row in login_info]
-    }
+    login_info = None
+    context = {}
+
+    if request.method == 'POST':
+        
+        if 'login' in request.POST:
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            # let's test the mySQL database connection
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM loginTest WHERE username = %s AND user_password = %s",
+                        [username, password])
+                login_info = cursor.fetchall()
+            # If you want to pass data into the html files, you include them here in context. 
+            # Context is in JSON so you can add multiple pieces of information
+            # check index.html to see how to display the information
+
+            if login_info:
+                message = "Log in successful!"
+            else:
+                message = "Invalid credentials"
+    
+            context['message'] = message
+            #return render(request, "railway/index.html", context)
+        
+        elif 'logout' in request.POST:
+            context['message'] = "Successfully logged out."
+            
     return render(request, "railway/index.html", context)
+
+
